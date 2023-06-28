8e8f3125b013740e99048fe56877ff5bc4d6e3ae
Asheeholic <chacha_95@naver.com>
2023-06-28 17:22:44 +0900

    vue add axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+// 추가
+// 앱 생성 실시
+const app = createApp(App)
+
+// axios
+app.config.globalProperties.$axios = axios;
+
+// vuetify
+app.use(vuetify);
+
+// mount
+app.mount('#app')
+
+
